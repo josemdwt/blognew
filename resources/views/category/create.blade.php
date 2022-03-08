838bf3f74b5183a90d0cd4cd1173ac4352b9405d
josemdwt <33197959+josemdwt@users.noreply.github.com>
2022-03-08 12:53:33 +0100

    add file input

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -22,6 +22,8 @@
                         <option value="{{ $category->id }}">{{  $category->name  }}</option>
                     @endforeach
                 </select>
+                <label class="text-sm font-bold uppercase opacity-70">Image</label>
+                <input type="file" name="image" class="w-full p-3 mt-2 mb-4 rounded bg-slate-200">
                 <input type="submit"
                     class="px-6 py-3 my-2 font-medium text-white duration-300 ease-in-out rounded cursor-pointer bg-emerald-500 hover:bg-indigo-500"
                     value="Send">
